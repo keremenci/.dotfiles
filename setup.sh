b1daced829c54ededf3db126a69baa49842f6cfa
keremenci <57116822+keremenci@users.noreply.github.com>
2023-09-18 15:00:05 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 #### WIP ####
 
+DOTFILES = $HOME/.dotfiles
+
 # install p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
@@ -12,8 +14,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 
 # zsh
 
-ln -s ./.zshrc ~/.zshrc
-ln -s ./.p10k.zsh ~/.p10k.zsh
+ln -s $DOTFILES/.zshrc ~/.zshrc
+ln -s $DOTFILES/.p10k.zsh ~/.p10k.zsh
 
 # vim
 
